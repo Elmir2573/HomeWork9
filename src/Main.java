a95d60c65a2549146243c126245107ae91c83498
zzr-i@mail.ru <zzr-i@mail.ru>
2024-12-09 21:53:28 +0300

    HW9 на проверку 2

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила" + totalSalary + " рублей");
         System.out.println();
-        System.out.println("Задание 2");
+        System.out.println("Задание №2");
         int[] cost = {586, 695, 458, 236, 547};
         int amountOfCosts = -1;
         for (int i = 0; i < cost.length; i++) {
@@ -26,7 +26,7 @@ public class Main {
         }
         System.out.println("Минимальная сумма трат за неделю составила " + amountOfCosts + " рублей.");
         System.out.println();
-        System.out.println("Задание 3");
+        System.out.println("Задание №3");
         int[] cost3 = {5863, 6695, 8458, 2236, 7547};
         int summ = 0;
         for (int i : cost3) {
@@ -35,7 +35,7 @@ public class Main {
         double average = (double) summ / cost3.length;
         System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
         System.out.println();
-        System.out.println("Задание 4");
+        System.out.println("Задание №4");
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
